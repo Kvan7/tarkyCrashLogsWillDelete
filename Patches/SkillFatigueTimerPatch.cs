f90eecd2f66b133d8a7623b0633000d2acbdf3e0
kvan7 <kvan.valve@gmail.com>
2024-07-14 14:14:47 -0500

    time remaining update observe dict

--- Patches/SkillFatigueTimerPatch.cs
+++ Patches/SkillFatigueTimerPatch.cs
@@ -4,20 +4,42 @@ using System.Collections.Generic;
 using System.Reflection;
 using EFT;
 using EFT.UI;
-using kvan.RaidSkillInfo.Controllers;
 using kvan.RaidSkillInfo.Helpers;
 using System;
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using System.Collections.ObjectModel;
 
 namespace kvan.RaidSkillInfo.Patches
 {
+	public class ObservableDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+	{
+		public event Action<TKey, TValue> OnValueChanged;
+
+		new public TValue this[TKey key]
+		{
+			get => base[key];
+			set
+			{
+				base[key] = value;
+				OnValueChanged?.Invoke(key, value);
+			}
+		}
+	}
 	internal class SkillFatigueTimerPatch : ModulePatch
 	{
-		public static readonly Dictionary<ESkillId, float> TimeRemaining = new Dictionary<ESkillId, float>();
+		public static readonly ObservableDictionary<ESkillId, float> TimeRemaining = new ObservableDictionary<ESkillId, float>();
+		private static readonly Dictionary<ESkillId, TextMeshProUGUI> fatigueTimerTexts = new Dictionary<ESkillId, TextMeshProUGUI>();
+
 		protected override MethodBase GetTargetMethod()
 		{
 			return AccessTools.Method(typeof(SkillPanel), nameof(SkillPanel.Show));
+		}
+
+		public SkillFatigueTimerPatch()
+		{
+			TimeRemaining.OnValueChanged += UpdateFatigueTimerText;
 		}
 
 		[PatchPostfix]
@@ -41,6 +63,17 @@ namespace kvan.RaidSkillInfo.Patches
 				return;
 			}
 
+			// Get the skill class to determine the skill ID
+			var skillClass = AccessTools.Field(typeof(SkillPanel), "skillClass").GetValue(__instance) as SkillClass;
+			if (skillClass == null) return;
+
+			// Remove old TextMeshProUGUI element if it exists
+			if (fatigueTimerTexts.TryGetValue(skillClass.Id, out var existingText))
+			{
+				GameObject.Destroy(existingText.gameObject);
+				fatigueTimerTexts.Remove(skillClass.Id);
+			}
+
 			// Create a new TextMeshProUGUI element
 			var fatigueTimerText = new GameObject("FatigueTimerText").AddComponent<TextMeshProUGUI>();
 			fatigueTimerText.transform.SetParent(effectivenessDownObject.transform, false);
@@ -52,21 +85,24 @@ namespace kvan.RaidSkillInfo.Patches
 			RectTransform rectTransform = fatigueTimerText.GetComponent<RectTransform>();
 			rectTransform.anchoredPosition = new Vector2(-20, 0);
 
-			// Get the skill class to determine the time remaining
-			var skillClass = AccessTools.Field(typeof(SkillPanel), "skillClass").GetValue(__instance) as SkillClass;
-			if (skillClass != null)
-			{
-				if (TimeRemaining.TryGetValue(skillClass.Id, out float timeRemaining))
-				{
-					fatigueTimerText.text = timeRemaining >= 0 && timeRemaining < 1e5f ? timeRemaining.ToString("F0") : string.Empty;
-				}
-			}
+			// Add to the dictionary
+			fatigueTimerTexts[skillClass.Id] = fatigueTimerText;
 
-			// Optionally position the text element as the first or last sibling
+			// Position the text element as the first or last sibling
 			fatigueTimerText.transform.SetAsLastSibling(); // or use SetAsFirstSibling() if you want it at the start
+
+			// Update the text element immediately
+			UpdateFatigueTimerText(skillClass.Id, TimeRemaining.ContainsKey(skillClass.Id) ? TimeRemaining[skillClass.Id] : 0f);
+		}
+
+		private static void UpdateFatigueTimerText(ESkillId skillId, float timeRemaining)
+		{
+			if (fatigueTimerTexts.TryGetValue(skillId, out var fatigueTimerText))
+			{
+				fatigueTimerText.text = timeRemaining >= 0 && timeRemaining < 1e5f ? timeRemaining.ToString("F0") : string.Empty;
+			}
 		}
 	}
-
 
 	internal class SkillFatigueTimerTooltipPatch : ModulePatch
 	{
